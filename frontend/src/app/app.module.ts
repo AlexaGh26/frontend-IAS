ca5a5e7fad20110030949b93d2f6bc118f26f357
Alexagh26 <alexagh26@gmail.com>
2020-10-12 20:04:20 -0500

    add css body principal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './pages/home/home.module';
+import { RegistrationModule } from './components/registration/registration.module';
+import { LayoutModule } from './components/layout/layout.module';
+import { CommonModule } from "@angular/common";
+import { TableModule } from './components/table/table.module';
+import { HistoryModule } from './components/history/history.module';
+import { ShowValueModule } from './components/show-value/show-value.module';
+import { ModalResultCreditModalModule } from './components/modal-result-credit/modal-result-credit.module';
+import {CreditService} from './services/credit.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +21,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HomeModule,
+    RegistrationModule,
+    LayoutModule,
+    CommonModule,
+    TableModule,
+    HistoryModule,
+    ShowValueModule,
+    ModalResultCreditModalModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
