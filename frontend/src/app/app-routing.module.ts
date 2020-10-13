d38928f1ef5652133b9a6bff12e4cad1c8c0eff5
Alexagh26 <alexagh26@gmail.com>
2020-10-12 19:54:56 -0500

    configuration routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HistoryComponent } from './components/history/history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent, // this is the component with the <router-outlet> in the template
+    children: [
+      {
+        path: 'home', // child route path
+        component: HomeComponent, // child route component that the router renders
+      },
+      {
+        path: 'registration',
+        component: RegistrationComponent, // another child route component that the router renders
+      },
+      {
+        path: 'history',
+        component: HistoryComponent, // another child route component that the router renders
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+
+ 
+}
