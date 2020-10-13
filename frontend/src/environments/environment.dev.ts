97c02ce8b0d04a0f0d75d1e33e8fb197a9a59082
Alexagh26 <alexagh26@gmail.com>
2020-10-12 19:58:25 -0500

    additional configurations

---
+++ frontend/src/environments/environment.dev.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    DEFAULTCREDITVALUE: 100000,
+    MINVALUE: 100000,
+    MAXVALUE: 1000000,
+    DENIED: 'Su solicitud fue negada, verifique su moto disponible o llamenos al +57(1)6331409',
+    APPROVAL: 'Su solicitud fue aprobada con èxito',
+    NOCREDIT: 'Lo sentimos su saldo disponible es 0',
+    AMOUNTCREDIT: 'Su saldo disponible es: ',
+    ZERO: 0,
+    BEATS: 'El valor solicitado supera el saldo disponible, intentelo de nuevo',
+    dev: true,
+  };
